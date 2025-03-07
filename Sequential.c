2223615dbb474bc0b4f6ff8eb08cc53c5c1d398a
Fatima Masood <1157fatima@gmail.com>
2025-03-07 19:40:49 +0500

    Getting started

---
+++ Sequential.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//We are going to find the nTotal number of odd numbers present in arrray.
+//For our sequential and parallel code to make clear difference we will choose an array million numbers
+#define N = 100000000
+
+
+int main(){
+
+    
+}
