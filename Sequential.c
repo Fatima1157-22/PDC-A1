661d43774c1e047ea439392208b5a9cd30ae9192
Fatima Masood <1157fatima@gmail.com>
2025-03-08 16:57:09 +0500

    Added execution time

--- Sequential.c
+++ Sequential.c
@@ -4,7 +4,7 @@
 
 //We are going to find the nTotal numbers greater than a threshold in an arrray.
 
-#define N = 100000000
+#define N 100000000
 #define THRESHOLD 500000
 
 
@@ -28,6 +28,34 @@ int main(){
         overflow. malloc() method helps us allocate the memory in heap which helps
         in handling large memory allocations. 
     */
+    int *array = (int *)malloc(N * sizeof(int));
+    if( array == NULL){
+        printf("Memory Allocation Failed!\n");
+        return 1;
+    }
+
+    //Initializing random number generator
+    srand(time(NULL));
+
+    //Filling array with random Numbers
+    for( int i = 0; i < N; i++){
+        array[i] = rand() % 1000000;
+    }
+
+    //Start measuring execution time
+    clock_t start_time = clock();
+
+    int count = maxNumbers(array, N, THRESHOLD);
+
+    //stop measuring execution time
+    clock_t end_time = clock();
+
+
+    double execution_time = (double)(end_time - start_time)/ CLOCKS_PER_SEC;
+
+    //printing the results
+    printf("Total numbers greater than threshold %d: %d\n", THRESHOLD, count);
+    printf("Execution Time for sequential code: %f seconds\n", execution_time);
 
 
 }
