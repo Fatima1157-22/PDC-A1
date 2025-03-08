54691b7da8478b999760109552818e1616c916e6
Fatima Masood <1157fatima@gmail.com>
2025-03-08 16:39:47 +0500

    Added a findind function

--- Sequential.c
+++ Sequential.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//We are going to find the nTotal number of odd numbers present in arrray.
-//For our sequential and parallel code to make clear difference we will choose an array million numbers
-#define N = 100000000
 
+//We are going to find the nTotal numbers greater than a threshold in an arrray.
+
+#define N = 100000000
+#define THRESHOLD 500000
+
+
+//Function to find number
+int maxNumbers(int *array, int size, int threshold){
+    //Variable to store the count
+    int count =0;
+
+    //Now we will loop through each number in an array
+    for( int i = 0; i < size; i++){
+        //checking if number present in an array is greater than our given threshold
+        if(array[i] > threshold){
+            count++;
+        }
+    }
+    return count;
+}
 
 int main(){
+    /* We are dynamically allocation our array as stack memory can lead to stack 
+        overflow. malloc() method helps us allocate the memory in heap which helps
+        in handling large memory allocations. 
+    */
 
-    
+
 }
