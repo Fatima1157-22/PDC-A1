e1e0c0df79daafe4d9c4f1ef4689f5c4d7a8db64
Fatima Masood <1157fatima@gmail.com>
2025-03-08 17:32:54 +0500

    Tried parallelizing code with CRTICAL

--- Parallel.c
+++ Parallel.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 //We are going to find the nTotal numbers greater than a threshold in an arrray.
 
@@ -8,15 +9,16 @@
 #define THRESHOLD 500000
 
 
-//Function to find number
-int maxNumbers(int *array, int size, int threshold){
+//Function to find number using OpenMp
+int maxNumbers(int *array, int size){
     //Variable to store the count
     int count =0;
 
-    //Now we will loop through each number in an array
+    //First try to get parallelization using Critical
+    #pragma omp parallel for schedule(static)
     for( int i = 0; i < size; i++){
-        //checking if number present in an array is greater than our given threshold
-        if(array[i] > threshold){
+        if(array[i] > THRESHOLD){
+            #pragma omp critical
             count++;
         }
     }
@@ -43,19 +45,17 @@ int main(){
     }
 
     //Start measuring execution time
-    clock_t start_time = clock();
+    double start_time = omp_get_wtime();
 
-    int count = maxNumbers(array, N, THRESHOLD);
+    int count = maxNumbers(array, N);
 
     //stop measuring execution time
-    clock_t end_time = clock();
+    double end_time = omp_get_wtime();
 
-
-    double execution_time = (double)(end_time - start_time)/ CLOCKS_PER_SEC;
 
     //printing the results
     printf("Total numbers greater than threshold %d: %d\n", THRESHOLD, count);
-    printf("Execution Time for sequential code: %f seconds\n", execution_time);
+    printf("Execution Time for sequential code: %f seconds\n",end_time - start_time);
 
     free(array);
     
