adff6bae7fc2ffe5526d63579a4c14f24bca5b24
Fatima Masood <1157fatima@gmail.com>
2025-03-21 15:43:54 +0500

    added different chunk sizes to code

--- Parallel.c
+++ Parallel.c
@@ -10,22 +10,22 @@
 
 
 //Function to find number using OpenMp
-int maxNumbers(int *array, int size){
+int maxNumbers(int *array, int size, int chunk_size){
     //Variable to store the count
     int count =0;
 
-    omp_set_num_threads(8);
+    //omp_set_num_threads(2);
 
     /*First try to get parallelization using Critical
-    Critical made the execution time go near 15 sec which is not even beteer than seq. 0.8 sec.
+    Critical made the execution time go near 15 sec which is not even beteer than seq. 0.8 sec.*/
     
-    #pragma omp parallel for schedule(static)
-    for( int i = 0; i < size; i++){
-        if(array[i] > THRESHOLD){
-            #pragma omp critical
-            count++;
-        }
-    }*/
+    // #pragma omp parallel for schedule(static)
+    // for( int i = 0; i < size; i++){
+    //     if(array[i] > THRESHOLD){
+    //         #pragma omp critical
+    //         count++;
+    //     }
+    // }
 
     //Doing atomic but execution time does not make any fine difference but better than critical not seq.
     // #pragma omp parallel for schedule(static)
@@ -37,10 +37,9 @@ int maxNumbers(int *array, int size){
     // }
 
     //Using reduction method and achieved the result at its best 0.091 sec.
-    #pragma omp parallel for reduction(+:count) schedule(dynamic)
+    #pragma omp parallel for reduction(+:count) schedule(static, chunk_size)
     for( int i = 0; i < size; i++){
         if(array[i] > THRESHOLD){
-            #pragma omp atomic
             count++;
         }
     }
@@ -65,19 +64,30 @@ int main(){
     for( int i = 0; i < N; i++){
         array[i] = rand() % 1000000;
     }
+    //defining different chunk sizes
+    int chunk_sizes[]= {1000,10000,100000,500000};
+    int num_chunks = sizeof(chunk_sizes)/sizeof(chunk_sizes[0]);
 
-    //Start measuring execution time
-    double start_time = omp_get_wtime();
-
-    int count = maxNumbers(array, N);
-
-    //stop measuring execution time
-    double end_time = omp_get_wtime();
+    printf("Chunk sizes\tExecution Time (seconds)\n");
+    printf("-------------------------------------\n");
 
 
-    //printing the results
-    printf("Total numbers greater than threshold %d: %d\n", THRESHOLD, count);
-    printf("Execution Time for sequential code: %f seconds\n",end_time - start_time);
+    for(int i =0; i <num_chunks; i++){
+
+        //Start measuring execution time
+        double start_time = omp_get_wtime();
+        int count = maxNumbers(array, N, chunk_sizes[i]);
+        
+        //stop measuring execution time
+        double end_time = omp_get_wtime();
+
+        //printing the results
+        printf("%d\t\t%f\n", chunk_sizes[i], end_time - start_time);
+    }
+    
+    
+    // printf("Total numbers greater than threshold %d: %d\n", THRESHOLD, count);
+    // printf("Execution Time for parallel code: %f seconds\n",end_time - start_time);
 
     free(array);
     
